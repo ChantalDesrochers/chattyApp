aa4d2aee78704a03e336faf0c4b1c4ace35a30ef
Chantal <chandesrochers@gmail.com>
2018-06-12 17:54:25 +0000

    passed in current user

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,27 @@ import NavBar from './NavBar.jsx';
 class App extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loading: true,
+      currentUser: {name: "Bob"},
+      messages: [
+      {
+      username: "Bob",
+      content: "Has anyone seen my marbles"
+      },
+      {
+      username: "Anonymous",
+      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+    }
+      ]
+    };
   }
   render() {
   return (
     <div>
     <NavBar />
-    <MessageList />
-    <ChatBar />
+    <MessageList messages={this.state.messages}/>
+    <ChatBar currentUser={this.state.currentUser.name} />
     </div>
     );
   }
