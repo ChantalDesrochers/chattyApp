16a8163235edba871842c3066a7d611cfec8d749
Chantal <chandesrochers@gmail.com>
2018-06-12 16:23:47 +0000

    added classes to make components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import React, {Component} from 'react';
+import MessageList from './MessageList.jsx';
+import ChatBar from './ChatBar.jsx';
+import NavBar from './NavBar.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
-    return (
-      <h1>Hello React :)</h1>
+  return (
+    <div>
+    <NavBar />
+    <MessageList />
+    <ChatBar />
+    </div>
     );
   }
 }
